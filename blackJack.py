abc5a239499aca3e5471ef43cbc40ef3f119eb66
Roachk <kroach41990@gmail.com>
2021-03-27 23:14:34 -0400

    Update blackJack.py

--- blackJack.py
+++ blackJack.py
@@ -1,9 +1,26 @@
 import random
 
-deck = ['A','A','A','A',2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','k','k']
+#deck = ['A','A','A','A',2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
+#Test Desk
+deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
+
+#List to hold players hands
 playerOneHand = []
 dealerHand = []
+tempDealerHand = []
+
+#List to do math with
+playerOneHandMath = []
+dealerHandMath = []
+
 players = [playerOneHand, dealerHand]
+playerMath = [playerOneHandMath, dealerHandMath]
+
+def twentyOneChecker(hand):
+    handTotal = 0
+    for i in range(0, len(hand)):
+        handTotal = handTotal + int(hand[i])
+        print(handTotal)
 
 def dealCards():
     deckLength = len(deck)
@@ -15,14 +32,36 @@ def dealCards():
     return cardPick
 
 def startGame():
+    #This for for loop simulates a 21 dealer dealing cards to each player one at a time.
     for i in range(len(players)):
         #print(i)
         for k in range(len(players)):
             #print(players[k])
             players[k].append(dealCards())
+            playerMath[k] = players[k]
+            
+    global tempDealerHand
+    tempDealerHand = dealerHand
+    del tempDealerHand[0]
+    tempDealerHand.insert(0,'*')
+    
+    #This for loop shows the cards of each player
     for i in range(len(players)):
-        print('Player ' + str((i + 1)) + ' showing')
-        print(players[i])
+        if players[i] == players[-1]:
+            print('Dealer is showing')
+            print(tempDealerHand)
+        else:
+            print('Player ' + str((i + 1)) + ' is showing')
+            print(players[i])
+        #sets playerMath list equal to the players list
+        
+    #Converts the players hands to numbers ie J = 10, Q = 10 ...
+    
+    #Checks if a player or dealer has 21
+    #for x in range(len(players)):
+        #print(players[x])
+        #handTotal = twentyOneChecker(players[x])
+        #input()
 
 print("Welcome to Kevin's Blackjack\nWould you like to play?\n(y)es (n)o (r)ules")
 playerStart = input()
@@ -32,5 +71,4 @@ if playerStart == 'y':
     startGame()
 else:
     print('Goodbye')
-
 
