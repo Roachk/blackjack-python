7cfff24704ce26a2c7fa92663269e24b19d8ad07
Roachk <kroach41990@gmail.com>
2021-03-28 05:40:16 +0000

    Writing Code

--- blackJack.py
+++ blackJack.py
@@ -1,8 +1,8 @@
 import random
 
-#deck = ['A','A','A','A',2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
+deck = ['A','A','A','A',2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
 #Test Desk
-deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
+#deck = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,'J','J','J','J','Q','Q','Q','Q','K','K','K','K']
 
 #List to hold players hands
 playerOneHand = []
@@ -17,10 +17,23 @@ players = [playerOneHand, dealerHand]
 playerMath = [playerOneHandMath, dealerHandMath]
 
 def twentyOneChecker(hand):
-    handTotal = 0
-    for i in range(0, len(hand)):
-        handTotal = handTotal + int(hand[i])
-        print(handTotal)
+  handTotal = 0
+  #print(hand)
+  for i in range(0, len(hand)):
+    handTotal = handTotal + int(hand[i])
+    #print(handTotal)
+  return handTotal
+
+def handConverter(hand):
+  #print(hand)
+  for i in range (len(hand)):
+    if hand[i] == 'J' or hand[i] == 'K' or hand[i] == 'Q':
+      hand[i] = 10
+    elif hand[i] == 'A':
+      hand[i] = 11
+  #print(hand)
+  return hand
+  #input()  
 
 def dealCards():
     deckLength = len(deck)
@@ -41,31 +54,46 @@ def startGame():
             playerMath[k] = players[k]
             
     global tempDealerHand
-    tempDealerHand = dealerHand
+    #This is to show the hidden dealerhand
+    for f in range(len(dealerHand)):
+      #print('test')
+      tempDealerHand.append(dealerHand[f])
+      #print(tempDealerHand)
+
     del tempDealerHand[0]
-    tempDealerHand.insert(0,'*')
+    tempDealerHand.insert(0, '*')  
     
     #This for loop shows the cards of each player
     for i in range(len(players)):
         if players[i] == players[-1]:
             print('Dealer is showing')
             print(tempDealerHand)
+            #print(dealerHand)
         else:
             print('Player ' + str((i + 1)) + ' is showing')
             print(players[i])
-        #sets playerMath list equal to the players list
-        
+        #sets playerMath list equal to the players list    
     #Converts the players hands to numbers ie J = 10, Q = 10 ...
+    for g in range(len(playerMath)):
+      #print(playerMath[g])
+      playerMath[g] = handConverter(playerMath[g])
+      #print(playerMath[g])
     
+    for y in range(len(playerMath)):
+      handTotal = twentyOneChecker(playerMath[y])
+      #print(handTotal)
+
+
     #Checks if a player or dealer has 21
     #for x in range(len(players)):
         #print(players[x])
         #handTotal = twentyOneChecker(players[x])
         #input()
-
+      
 print("Welcome to Kevin's Blackjack\nWould you like to play?\n(y)es (n)o (r)ules")
 playerStart = input()
 
+#Zydrate comes in a little glass vile
 if playerStart == 'y':
     print('Dealing Cards')
     startGame()
